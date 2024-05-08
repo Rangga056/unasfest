4cc8e19cd7990fe5185a61327949501571414c18
Ferdiansyach <iyanferdiansyach30@gmail.com>
2024-05-08 08:43:52 +0700

    change constant path

--- constants/navMenu.tsx
+++ constants/navMenu.tsx
@@ -1,45 +1,44 @@
 export const navMenu = [
-    {
-      path: "/",
-      title: "Beranda",
-    },
-    {
-      path: "/kegiatan",
-      title: "Kegiatan",
-      child: [
-        {
-          name: "Kompetisi Debat Bahasa Indonesia (KDBI)",
-          href: "/kegiatan/KDBI",
-        },
-        {
-          name: "English Debate Competition (EDC)",
-          href: "/kegiatan/EDC",
-        },
-        {
-          name: "Lomba Karya Tulis Ilmiah",
-          href: "/kegiatan/LKTI",
-        },
-        {
-          name: "Seminar Nasional",
-          href: "/kegiatan/SEMNAS",
-        },
-        {
-          name: "Short Movie",
-          href: "/kegiatan/ShortMovie",
-        },
-      ],
-    },
-    {
-      path: "/galeri",
-      title: "Galeri",
-    },
-    {
-      path: "/sponsorship",
-      title: "Sponsorship",
-    },
-    {
-      path: "/tentang",
-      title: "Tentang",
-    },
-  ];
-  
+  {
+    path: "/",
+    title: "Beranda",
+  },
+  {
+    path: "/activities",
+    title: "Activities",
+    child: [
+      {
+        name: "Kompetisi Debat Bahasa Indonesia (KDBI)",
+        href: "/kegiatan/KDBI",
+      },
+      {
+        name: "English Debate Competition (EDC)",
+        href: "/kegiatan/EDC",
+      },
+      {
+        name: "Lomba Karya Tulis Ilmiah",
+        href: "/kegiatan/LKTI",
+      },
+      {
+        name: "Seminar Nasional",
+        href: "/kegiatan/SEMNAS",
+      },
+      {
+        name: "Short Movie",
+        href: "/kegiatan/ShortMovie",
+      },
+    ],
+  },
+  {
+    path: "/galeri",
+    title: "Galeri",
+  },
+  {
+    path: "/sponsorship",
+    title: "Sponsorship",
+  },
+  {
+    path: "/tentang",
+    title: "Tentang",
+  },
+];
