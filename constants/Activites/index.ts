f7916b07125950a2a99690cdc460ff7946c04d9b
Ferdiansyach <iyanferdiansyach30@gmail.com>
2024-05-12 20:54:39 +0700

    fix footer: -logo icon gmail & location -copyright simbol -ubah data di constant

--- constants/Activites/index.ts
+++ constants/Activites/index.ts
@@ -1,170 +1,172 @@
 //cover Image
-import kdbiCover from "@/public/assets/images/competition/kdbiCover.png"
-import edcCover from "@/public/assets/images/competition/edcCover.png"
-import scpCover from "@/public/assets/images/competition/scpCover.png"
-import smcCover from "@/public/assets/images/competition/smcCover.png"
+import kdbiCover from "@/public/assets/images/competition/kdbiCover.png";
+import edcCover from "@/public/assets/images/competition/edcCover.png";
+import scpCover from "@/public/assets/images/competition/scpCover.png";
+import smcCover from "@/public/assets/images/competition/smcCover.png";
 
 //timeline
-import {timelinesEDC, timelinesKDBI, timelinesSPC, timelinesSMC} from "@/constants/Timeline"
+import {
+  timelinesEDC,
+  timelinesKDBI,
+  timelinesSPC,
+  timelinesSMC,
+} from "@/constants/Timeline";
 export const Activities = [
-    {
-        path: "kdbi",
-        title: "Competisi Debat Bahasa Indonesia",
-        coverImage : kdbiCover,
-        color: "#2A9763",
-        description: "The English Debate Competition (EDC) is an English language debate competition where participants debate arguments and thoughts related to contemporary issues from various aspects, namely social, political, cultural, technological, economic, energy, legal, health and so on. Therefore, the English Debate Competition (EDC) aims to improve participants' ability to speak in public, develop analytical and critical thinking skills, and strengthen their ability to argue using effective and persuasive language. The English Debate Competition (EDC) will be held with the theme 'Implementation of Technology in Blue Econo...",
-        timeline: timelinesKDBI,
-        guideBook : "https://ufest.unas.ac.id/",
-        requirementsTitle: "Indonesian Debate Competition requirements",
-        requirements: [
-            {
-                no: "01",
-                provision : "Students who register themselves on the registration link that has been listed on the UNAS FEST 2023 website."
+  {
+    path: "kdbi",
+    title: "Competisi Debat Bahasa Indonesia",
+    coverImage: kdbiCover,
+    color: "#2A9763",
+    description:
+      "The English Debate Competition (EDC) is an English language debate competition where participants debate arguments and thoughts related to contemporary issues from various aspects, namely social, political, cultural, technological, economic, energy, legal, health and so on. Therefore, the English Debate Competition (EDC) aims to improve participants' ability to speak in public, develop analytical and critical thinking skills, and strengthen their ability to argue using effective and persuasive language. The English Debate Competition (EDC) will be held with the theme 'Implementation of Technology in Blue Econo...",
+    timeline: timelinesKDBI,
+    guideBook: "https://ufest.unas.ac.id/",
+    requirementsTitle: "Indonesian Debate Competition requirements",
+    requirements: [
+      {
+        no: "01",
+        provision:
+          "Students who register themselves on the registration link that has been listed on the UNAS FEST 2023 website.",
+      },
+      {
+        no: "02",
+        provision:
+          "Active students of Bachelor and Diploma programs registered in PD-DIKTI (Higher Education Database) from various study programs and public and private universities..",
+      },
+      {
+        no: "03",
+        provision:
+          "Debate participants are students who have never had a Bachelor (S1) / Applied Bachelor (D4) degree.",
+      },
+      {
+        no: "04",
+        provision:
+          "Students follow the rules that have been determined and listed in the KDBI Unas Fest 2023 activity guidebook.",
+      },
+      {
+        no: "05",
+        provision:
+          "Active students of Bachelor and Diploma programs registered in PD-DIKTI (Higher Education Database) from various study programs and public and private universities..",
+      },
+    ],
+  },
 
-            },
-            {
-                no: "02",
-                provision : "Active students of Bachelor and Diploma programs registered in PD-DIKTI (Higher Education Database) from various study programs and public and private universities.."
+  {
+    path: "edc",
+    title: "english debate competition",
+    coverImage: edcCover,
+    color: "#004AAD",
+    description:
+      "The English Debate Competition (EDC) is an English language debate competition where participants debate arguments and thoughts related to contemporary issues from various aspects, namely social, political, cultural, technological, economic, energy, legal, health and so on. Therefore, the English Debate Competition (EDC) aims to improve participants' ability to speak in public, develop analytical and critical thinking skills, and strengthen their ability to argue using effective and persuasive language. The English Debate Competition (EDC) will be held with the theme 'Implementation of Technology in Blue Econo...",
+    timeline: timelinesEDC,
+    guideBook: "https://ufest.unas.ac.id/",
+    requirementsTitle: "Indonesian Deabte Competition requirements",
+    requirements: [
+      {
+        no: "01",
+        provision:
+          "Students who register themselves on the registration link that has been listed on the UNAS FEST 2023 website.",
+      },
+      {
+        no: "02",
+        provision:
+          "Active students of Bachelor and Diploma programs registered in PD-DIKTI (Higher Education Database) from various study programs and public and private universities..",
+      },
+      {
+        no: "03",
+        provision:
+          "Debate participants are students who have never had a Bachelor (S1) / Applied Bachelor (D4) degree.",
+      },
+      {
+        no: "04",
+        provision:
+          "Students follow the rules that have been determined and listed in the KDBI Unas Fest 2023 activity guidebook.",
+      },
+      {
+        no: "05",
+        provision:
+          "Active students of Bachelor and Diploma programs registered in PD-DIKTI (Higher Education Database) from various study programs and public and private universities..",
+      },
+    ],
+  },
 
-            },
-            {
-                no: "03",
-                provision : "Debate participants are students who have never had a Bachelor (S1) / Applied Bachelor (D4) degree."
+  {
+    path: "spc",
+    title: "scintific paper competition",
+    coverImage: scpCover,
+    color: "#C02424",
+    description:
+      "The English Debate Competition (EDC) is an English language debate competition where participants debate arguments and thoughts related to contemporary issues from various aspects, namely social, political, cultural, technological, economic, energy, legal, health and so on. Therefore, the English Debate Competition (EDC) aims to improve participants' ability to speak in public, develop analytical and critical thinking skills, and strengthen their ability to argue using effective and persuasive language. The English Debate Competition (EDC) will be held with the theme 'Implementation of Technology in Blue Econo...",
+    timeline: timelinesSPC,
+    guideBook: "https://ufest.unas.ac.id/",
+    requirementsTitle: "Indonesian Deabte Competition requirements",
 
-            },
-            {
-                no: "04",
-                provision : "Students follow the rules that have been determined and listed in the KDBI Unas Fest 2023 activity guidebook."
+    requirements: [
+      {
+        no: "01",
+        provision:
+          "Students who register themselves on the registration link that has been listed on the UNAS FEST 2023 website.",
+      },
+      {
+        no: "02",
+        provision:
+          "Active students of Bachelor and Diploma programs registered in PD-DIKTI (Higher Education Database) from various study programs and public and private universities..",
+      },
+      {
+        no: "03",
+        provision:
+          "Debate participants are students who have never had a Bachelor (S1) / Applied Bachelor (D4) degree.",
+      },
+      {
+        no: "04",
+        provision:
+          "Students follow the rules that have been determined and listed in the KDBI Unas Fest 2023 activity guidebook.",
+      },
+      {
+        no: "05",
+        provision:
+          "Active students of Bachelor and Diploma programs registered in PD-DIKTI (Higher Education Database) from various study programs and public and private universities..",
+      },
+    ],
+  },
 
-            },
-            {
-                no: "05",
-                provision : "Active students of Bachelor and Diploma programs registered in PD-DIKTI (Higher Education Database) from various study programs and public and private universities.."
+  {
+    path: "smc",
+    title: "short movie competition",
+    coverImage: smcCover,
+    color: "#E27817",
+    description:
+      "The English Debate Competition (EDC) is an English language debate competition where participants debate arguments and thoughts related to contemporary issues from various aspects, namely social, political, cultural, technological, economic, energy, legal, health and so on. Therefore, the English Debate Competition (EDC) aims to improve participants' ability to speak in public, develop analytical and critical thinking skills, and strengthen their ability to argue using effective and persuasive language. The English Debate Competition (EDC) will be held with the theme 'Implementation of Technology in Blue Econo...",
+    timeline: timelinesSMC,
+    guideBook: "https://ufest.unas.ac.id/",
+    requirementsTitle: "Indonesian Deabte Competition requirements",
 
-            },
-        ]
-
-    },
-
-
-    {
-        path: "edc",
-        title: "english debate competition",
-        coverImage : edcCover,
-        color: "#004AAD",
-        description: "The English Debate Competition (EDC) is an English language debate competition where participants debate arguments and thoughts related to contemporary issues from various aspects, namely social, political, cultural, technological, economic, energy, legal, health and so on. Therefore, the English Debate Competition (EDC) aims to improve participants' ability to speak in public, develop analytical and critical thinking skills, and strengthen their ability to argue using effective and persuasive language. The English Debate Competition (EDC) will be held with the theme 'Implementation of Technology in Blue Econo...",
-        timeline: timelinesEDC,
-        guideBook : "https://ufest.unas.ac.id/",
-        requirementsTitle: "Indonesian Deabte Competition requirements",
-        requirements: [
-            {
-                no: "01",
-                provision : "Students who register themselves on the registration link that has been listed on the UNAS FEST 2023 website."
-
-            },
-            {
-                no: "02",
-                provision : "Active students of Bachelor and Diploma programs registered in PD-DIKTI (Higher Education Database) from various study programs and public and private universities.."
-
-            },
-            {
-                no: "03",
-                provision : "Debate participants are students who have never had a Bachelor (S1) / Applied Bachelor (D4) degree."
-
-            },
-            {
-                no: "04",
-                provision : "Students follow the rules that have been determined and listed in the KDBI Unas Fest 2023 activity guidebook."
-
-            },
-            {
-                no: "05",
-                provision : "Active students of Bachelor and Diploma programs registered in PD-DIKTI (Higher Education Database) from various study programs and public and private universities.."
-
-            },
-        ]
-
-    },
-
-
-    {
-        path: "spc",
-        title: "scintific paper competition",
-        coverImage : scpCover,
-        color: "#C02424",
-        description: "The English Debate Competition (EDC) is an English language debate competition where participants debate arguments and thoughts related to contemporary issues from various aspects, namely social, political, cultural, technological, economic, energy, legal, health and so on. Therefore, the English Debate Competition (EDC) aims to improve participants' ability to speak in public, develop analytical and critical thinking skills, and strengthen their ability to argue using effective and persuasive language. The English Debate Competition (EDC) will be held with the theme 'Implementation of Technology in Blue Econo...",
-        timeline: timelinesSPC,
-        guideBook : "https://ufest.unas.ac.id/",
-        requirementsTitle: "Indonesian Deabte Competition requirements",
-
-        requirements: [
-            {
-                no: "01",
-                provision : "Students who register themselves on the registration link that has been listed on the UNAS FEST 2023 website."
-
-            },
-            {
-                no: "02",
-                provision : "Active students of Bachelor and Diploma programs registered in PD-DIKTI (Higher Education Database) from various study programs and public and private universities.."
-
-            },
-            {
-                no: "03",
-                provision : "Debate participants are students who have never had a Bachelor (S1) / Applied Bachelor (D4) degree."
-
-            },
-            {
-                no: "04",
-                provision : "Students follow the rules that have been determined and listed in the KDBI Unas Fest 2023 activity guidebook."
-
-            },
-            {
-                no: "05",
-                provision : "Active students of Bachelor and Diploma programs registered in PD-DIKTI (Higher Education Database) from various study programs and public and private universities.."
-
-            },
-        ]
-
-    },
-
-
-    {
-        path: "smc",
-        title: "short movie competition",
-        coverImage : smcCover,
-        color: "#E27817",
-        description: "The English Debate Competition (EDC) is an English language debate competition where participants debate arguments and thoughts related to contemporary issues from various aspects, namely social, political, cultural, technological, economic, energy, legal, health and so on. Therefore, the English Debate Competition (EDC) aims to improve participants' ability to speak in public, develop analytical and critical thinking skills, and strengthen their ability to argue using effective and persuasive language. The English Debate Competition (EDC) will be held with the theme 'Implementation of Technology in Blue Econo...",
-        timeline: timelinesSMC,
-        guideBook : "https://ufest.unas.ac.id/",
-        requirementsTitle: "Indonesian Deabte Competition requirements",
-
-        requirements: [
-            {
-                no: "01",
-                provision : "Students who register themselves on the registration link that has been listed on the UNAS FEST 2023 website."
-
-            },
-            {
-                no: "02",
-                provision : "Active students of Bachelor and Diploma programs registered in PD-DIKTI (Higher Education Database) from various study programs and public and private universities.."
-
-            },
-            {
-                no: "03",
-                provision : "Debate participants are students who have never had a Bachelor (S1) / Applied Bachelor (D4) degree."
-
-            },
-            {
-                no: "04",
-                provision : "Students follow the rules that have been determined and listed in the KDBI Unas Fest 2023 activity guidebook."
-
-            },
-            {
-                no: "05",
-                provision : "Active students of Bachelor and Diploma programs registered in PD-DIKTI (Higher Education Database) from various study programs and public and private universities.."
-
-            },
-        ]
-
-    },
-]
+    requirements: [
+      {
+        no: "01",
+        provision:
+          "Students who register themselves on the registration link that has been listed on the UNAS FEST 2023 website.",
+      },
+      {
+        no: "02",
+        provision:
+          "Active students of Bachelor and Diploma programs registered in PD-DIKTI (Higher Education Database) from various study programs and public and private universities..",
+      },
+      {
+        no: "03",
+        provision:
+          "Debate participants are students who have never had a Bachelor (S1) / Applied Bachelor (D4) degree.",
+      },
+      {
+        no: "04",
+        provision:
+          "Students follow the rules that have been determined and listed in the KDBI Unas Fest 2023 activity guidebook.",
+      },
+      {
+        no: "05",
+        provision:
+          "Active students of Bachelor and Diploma programs registered in PD-DIKTI (Higher Education Database) from various study programs and public and private universities..",
+      },
+    ],
+  },
+];
