fe7fa2b6e9083a27de5fa714c177770524c5e0db
RafindraAditias <rafindra.aditias@gmail.com>
2024-05-14 11:23:49 +0700

    update ui

--- components/ui/accordiion.tsx
+++ components/ui/accordiion.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as AccordionPrimitive from "@radix-ui/react-accordion";
-import { Plus } from "lucide-react";
+import { ChevronDown } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 
@@ -33,9 +33,9 @@ const AccordionTrigger = React.forwardRef<
       {...props}
     >
       {children}
-      <Plus
-        color="#1A0F91"
-        className="h-4 w-4 shrink-0 transition-transform duration-200"
+      <ChevronDown
+        className="text-violet10 ease-[cubic-bezier(0.87,_0,_0.13,_1)] transition-transform duration-300 group-data-[state=open]:rotate-180"
+        aria-hidden
       />
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
