48e236cab793ee7061de561fef5fcdbfec6e951a
Ferdiansyach <iyanferdiansyach30@gmail.com>
2024-05-20 19:05:06 +0700

    add icon on benefits

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ import { postersData } from "@/constants/posterDatas";
 import { faqsData } from "@/constants/Faqs";
 //TODO:Change the icon based on the figma design
 import HomeIcon from "@/public/assets/icons/unasfest-icon-logo.png";
+import SpecialBanner from "@/components/shared/Home/SpecialBanner";
 
 export default function Home() {
   const InfiniteSlidingProps = {
@@ -33,7 +34,11 @@ export default function Home() {
       {/* Benefit  */}
       <Benefit />
       {/* Gallery Component  */}
-      <Gallery />
+      <div className="max-w-[1400px] mx-auto px-3 md:container mt-20">
+        <Gallery />
+      </div>
+      {/* Special Banner */}
+      <SpecialBanner/>
       {/* Timeline Component */}
       <Timeline />
       {/* FAQ Component   */}
--- components/ui/benefitCard.tsx
+++ components/ui/benefitCard.tsx
@@ -1,6 +1,10 @@
 "use client";
-import certificate from "@/public/assets/images/home/certificate.png"
-import money from "@/public/assets/images/home/money.png"
+import certificate from "@/public/assets/images/home/certificate.png";
+import money from "@/public/assets/images/home/money.png";
+import merch from "@/public/assets/icons/benefit/merch.png";
+import trophy from "@/public/assets/icons/benefit/trophy.png";
+import plaque from "@/public/assets/icons/benefit/plaque.png";
+
 import Image from "next/image";
 
 import {
@@ -26,14 +30,39 @@ function BenefitCard({ title, children }: benefitProps) {
           {title}
         </CardTitle>
         <CardContent className="w-full h-full flex justify-around py-10 align-center text-xl md:p-20">
+          <div className="grid grid-cols-3 gap-20">
             <div className="space-y-5 flex flex-col items-center justify-between">
-              <Image src={certificate} alt="Certificate" className="w-10 lg:w-20"/>
-              <h1 className="text-xs md:text-2xl"> E-CERTIFICATE</h1>
+              <Image
+                src={certificate}
+                alt="Certificate"
+                className="w-10 lg:w-[71px]"
+              />
+              <h1 className="text-xs md:text-2xl text-center">
+                Certificate &<br />
+                E-Certificate
+              </h1>
             </div>
-            <div className="space-y-5 flex flex-col items-center justify-between">
-              <Image src={money} alt="Money" className="w-12 lg:w-24"/>
-              <h1 className="text-xs md:text-2xl"> MILIONS OF RUPIAH</h1>
+            <div className="space-y-5 flex flex-col items-center justify-center">
+              <Image src={money} alt="Money" className="w-12 lg:w-[95px]" />
+              <h1 className="text-xs md:text-2xl text-center">
+                Millions of Rupiah
+              </h1>
             </div>
+            <div className="space-y-5 flex flex-col items-center justify-center">
+              <Image src={trophy} alt="Trophy" className="w-12 lg:w-[80px]" />
+              <h1 className="text-xs md:text-2xl text-center">Trophy</h1>
+            </div>
+            <div className="flex justify-evenly items-center w-full col-span-3">
+              <div className="space-y-5 flex flex-col items-center justify-center">
+                <Image src={plaque} alt="Plaque" className="w-12 lg:w-[41]" />
+                <h1 className="text-xs md:text-2xl text-center">Plaque</h1>
+              </div>
+              <div className="space-y-5 flex flex-col items-center justify-center">
+                <Image src={merch} alt="Merch" className="w-12 lg:w-[77px]" />
+                <h1 className="text-xs md:text-2xl text-center">Merch</h1>
+              </div>
+            </div>
+          </div>
         </CardContent>
         <CardFooter className="hidden lg:h-2 lg:p-0 lg:w-3/5 lg:m-auto"></CardFooter>
       </Card>
