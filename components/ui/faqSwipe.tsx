a4dbc03a1f39a5a2d7732efd58f89d4be9661d81
RafindraAditias <rafindra.aditias@gmail.com>
2024-05-21 16:04:08 +0700

    responsive faq home

--- components/ui/faqSwipe.tsx
+++ components/ui/faqSwipe.tsx
@@ -1,15 +1,20 @@
 "use client";
+
+import React, { useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
+
 import styles from "@/components/styles/faq.module.css";
-import 'swiper/css';
-import "swiper/css/pagination";
+import "swiper/css";
 import { faqsDataProps } from "@/lib/types/Faq";
 import { Pagination, Navigation } from "swiper/modules";
 import { MoveRight } from "lucide-react";
 
-
-
 const FaqSwipe = ({ faqsData }: faqsDataProps) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleSlideChange = (index: number) => {
+    setActiveIndex(index);
+  };
 
   return (
     <div>
@@ -18,7 +23,7 @@ const FaqSwipe = ({ faqsData }: faqsDataProps) => {
         spaceBetween={10}
         pagination={{
           clickable: true,
-          el: ".custom-pagination",
+          el: ".custom-pagination-home1",
         }}
         navigation={{
           nextEl: ".swiper-button-next",
@@ -34,20 +39,24 @@ const FaqSwipe = ({ faqsData }: faqsDataProps) => {
               key={faq.index}
               className={`${styles.swiperSlide}`}
             >
-              <div className="text-left flex flex-col justify-around items-center align-middle m-2 p-9 font-inter h-[250px] text-white bg-[#004AAD] rounded-sm">
-                <div className="font-semibold text-xl w-full">{faq.quetion}</div>
-                <p className="font-bungee text-4xl w-full">“</p>
-                <div className="text-base w-full">{faq.answer}</div>
+              <div className="m-2 flex h-[250px] flex-col items-center justify-around rounded-sm bg-[#004AAD] p-9 text-left align-middle font-inter text-white">
+                <div className="w-full text-lg font-semibold md:text-xl lg:text-2xl">
+                  {faq.quetion}
+                </div>
+                <p className="w-full font-bungee text-4xl">“</p>
+                <div className="w-full text-sm md:text-base lg:text-xl">
+                  {faq.answer}
+                </div>
               </div>
             </SwiperSlide>
           );
         })}
       </Swiper>
 
-      <div className="absolute -bottom-10 left-0 z-10 flex items-center space-x-4 h-28">
-        <div className="custom-pagination-home" />
+      <div className="absolute -bottom-10 left-0 z-10 flex h-28 items-center space-x-4">
+        <div className="custom-pagination-home1" />
         <div className="swiper-button-next cursor-pointer bg-transparent p-2">
-          <MoveRight size={40} className="text-page-black" />
+          <MoveRight size={30} className="text-page-black" />
         </div>
       </div>
     </div>
