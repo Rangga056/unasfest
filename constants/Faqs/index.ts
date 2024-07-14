cbab1d78c0105b80c4fcafd65d6d733e5f721623
Ferdiansyach <iyanferdiansyach30@gmail.com>
2024-07-14 14:04:06 +0700

    fix FAQ,Navbar,Timeline

--- constants/Faqs/index.ts
+++ constants/Faqs/index.ts
@@ -1,38 +1,32 @@
 export const faqsData = [
-  {
-    index: 0,
-    quetion: "How do I register for the competition of UNAS FEST?",
-    answer:
-      "Visit the CATURNAWA website, go to 'Indonesian Language Debate Competition', click 'Register Now', fill the form, review, and make the payment",
-  },
-  {
-    index: 1,
-    quetion: "How do I register for the competition of UNAS FEST?",
-    answer:
-      "Click the posters at the top of the homepage to register for the competition or click the 'Register' button on the activity page",
-  },
-  {
-    index: 2,
-    quetion: "How much is the registration fee for the UNAS FEST competition?",
-    answer:
-      "The registration fees vary across competitions, starting from Rp 300,000 to Rp 550,000",
-  },
-  {
-    index: 3,
-    quetion: "When will the winners be announced?",
-    answer:
-      "The deadline for registration for the Kompetisi Debat Bahasa Indonesia (KDBI) is August 23, 2024",
-  },
-  {
-    index: 4,
-    quetion: "When will the winners be announced?",
-    answer:
-      "The winners will be announced on October 17, 2024, after the final round sessions",
-  },
-  {
-    index: 5,
-    quetion: "Where can I see the winner announcement?",
-    answer:
-      "The winner announcement will be available on this <a href='https://caturnawa.com'>website</a>",
-  },
-];
+    {
+        index: 0,
+        quetion: "How do I register for the competition of UNAS FEST?",
+        answer: "Visit the CATURNAWA website, go to 'Indonesian Language Debate Competition', click 'Register Now', fill the form, review, and make the payment",
+    },
+    {
+        index: 1,
+        quetion: "How do I register for the competition of UNAS FEST?",
+        answer: "Click the posters at the top of the homepage to register for the competition or click the 'Register' button on the activity page",
+    },
+    {
+        index: 2,
+        quetion: "How much is the registration fee for the UNAS FEST competition?",
+        answer: "The registration fees vary across competitions, starting from Rp 300,000 to Rp 550,000",
+    },
+    {
+        index: 3,
+        quetion: "When will the winners be announced?",
+        answer: "The deadline for registration for the Kompetisi Debat Bahasa Indonesia (KDBI) is August 23, 2024",
+    },
+    {
+        index: 4,
+        quetion: "When will the winners be announced?",
+        answer: "The winners will be announced on October 17, 2024, after the final round sessions",
+    },
+    {
+        index: 5,
+        quetion: "Where can I see the winner announcement?",
+        answer: "The winner announcement will be available on this <a id='caturnawa' href='https://caturnawa.com'>website</a>",
+    },
+]
