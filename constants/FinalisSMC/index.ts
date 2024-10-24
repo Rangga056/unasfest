cb87791d6ed93abb77c872dcb2d21d0dd84cbfe9
Ferdiansyach <iyanferdiansyach30@gmail.com>
2024-10-24 14:31:30 +0700

    component poster smc homepages

---
+++ constants/FinalisSMC/index.ts
@@ -0,0 +1,22 @@
+import dummyposter from "@/public/assets/images/home/posters/PosterSMC.jpg";
+
+export const FinalisSMC = [
+  {
+    index: 0,
+    poster: dummyposter,
+    title: "Lorem ipsum dolor sit",
+    team: "Lorem ipsum dolor sit",
+  },
+  {
+    index: 1,
+    poster: dummyposter,
+    title: "Lorem ipsum dolor sit ",
+    team: "Lorem ipsum dolor sit ",
+  },
+  {
+    index: 2,
+    poster: dummyposter,
+    title: "Lorem ipsum dolor sit ",
+    team: "Lorem ipsum dolor sit ",
+  },
+];
