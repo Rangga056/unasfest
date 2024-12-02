71a51f24e11ba88294e3f441c337ca667672e74f
Ferdiansyach <iyanferdiansyach30@gmail.com>
2024-12-02 10:50:15 +0700

    add Sponsor

--- constants/Partnership/index.ts
+++ constants/Partnership/index.ts
@@ -15,125 +15,190 @@ import mujigae from "@/public/assets/images/Partnership/mujigae.png";
 import aliansi from "@/public/assets/images/Partnership/aliansi.png";
 import antangin from "@/public/assets/images/Partnership/antangin.png";
 import momita from "@/public/assets/images/Partnership/momita_catering.png";
+import kemkomarvest from "@/public/assets/images/Partnership/KEMKOMARVEST.png";
+import kemkominfo from "@/public/assets/images/Partnership/KEMKOMINFO.png";
+import esdm from "@/public/assets/images/Partnership/ESDM.png";
+import omg from "@/public/assets/images/Partnership/OMG.png";
+import gpnuku from "@/public/assets/images/Partnership/GPNUKU.png";
+import sopikanternate from "@/public/assets/images/Partnership/SOPIKANTERNATE.png";
 
 export const partnership = [
-    {
-        index: 0,
-        image: sariroti,
-        title: "Sari Roti",
-        description: "Sari Roti produces and distributes a variety of high-quality, hygienic, and affordable halal breads for all Indonesian consumers.",
-        link: "https://www.sariroti.com/id"
-    },
-    {
-        index: 1,
-        image: byU,
-        title: "By.U",
-        description: "by.U is a fully digital internet SIM card from Telkomsel. All services are available in one app. Just open the by.U app to connect with the people and things you love.",
-        link: "https://www.byu.id/v2"
-    },
-    {
-        index: 2,
-        image: bumiaksara,
-        title: "Bumi Aksara",
-        description: "PT Bumi Aksara is a national-scale publishing company that has been trusted to publish quality books for over three decades.",
-        link: "https://bumiaksaraonline.com/Tentang-Kami"
-    },
-    {
-        index: 3,
-        image: buynana,
-        title: "Buynana Chips",
-        description: "Buynana Chips is a specialty banana chips product from Cianjur, available in five flavors: Chocolate, Milk, Strawberry, Original, and Green Tea.",
-        link: "https://buynanachips.com/"
-    },
-    {
-        index: 4,
-        image: monarq,
-        title: "Monarq Studio",
-        description: "Monarq Studio is a creative studio focused on innovative design and branding solutions for various industries.",
-        link: "https://www.instagram.com/monarqstudiom/"
-    },
-    {
-        index: 5,
-        image: bankBNI,
-        title: "Bank BNI",
-        description: "Bank BNI is one of the leading banks in Indonesia, offering a wide range of financial services for individuals and corporations.",
-        link: "https://www.bni.co.id/en-us/"
-    },
-    {
-        index: 6,
-        image: bankBRI,
-        title: "Bank BRI",
-        description: "Bank BRI is a bank that provides integrated banking services, focusing on empowering SMEs in Indonesia.",
-        link: "https://bri.co.id"
-    },
-    {
-        index: 7,
-        image: bankBTN,
-        title: "Bank BTN",
-        description: "Bank BTN is a bank known as a financial institution for housing in Indonesia, offering various banking products and services.",
-        link: "https://www.btn.co.id"
-    },
-    {
-        index: 8,
-        image: bankDKI,
-        title: "Bank DKI",
-        description: "Bank DKI is a regional bank operating in Jakarta, providing banking services for individuals and businesses.",
-        link: "https://www.bankdki.co.id"
-    },
-    {
-        index: 9,
-        image: baznas,
-        title: "BAZNAS RI",
-        description: "BAZNAS is the national zakat management agency tasked with managing and distributing zakat, infaq, and charity for the welfare of society.",
-        link: "https://baznas.go.id"
-    },
-    {
-        index: 10,
-        image: antangin,
-        title: "Antangin",
-        description: "Antangin provides herbal health products to boost immunity and provide relief.",
-        link: "https://www.deltomed.id"
-    },
-    {
-        index: 11,
-        image: aliansi,
-        title: "Aliansi Kebangsaan",
-        description: "Aliansi Kebangsaan promotes national unity and social development.",
-        link: "https://www.aliansikebangsaan.org/amp/"
-    },
-    {
-        index: 12,
-        image: makarizo,
-        title: "Makarizo",
-        description: "Makarizo is known for its high-quality hair care products.",
-        link: "https://makarizo.com"
-    },
-    {
-        index: 13,
-        image: mujigae,
-        title: "Mujigae",
-        description: "Mujigae offers a range of delicious Korean food products.",
-        link: "https://wonhaefoods.com/wonhae-series/"
-    },
-    {
-        index: 14,
-        image: momita,
-        title: "Momita Catering",
-        description: "Momita Catering offers a variety of delicious catering services for events.",
-        link: "https://www.instagram.com/momita_catering/"
-    },
-    {
-        index: 15,
-        image: weRent,
-        title: "We.Rent",
-        description: "We.Rent is a rental platform for various products and services, offering convenience for renting according to your needs.",
-        link: "https://www.instagram.com/we.rentjakarta/"
-    },
-    {
-        index: 16,
-        image: kebab,
-        title: "kebab monster",
-        description: "Kebab Monster menyajikan kebab lezat dengan porsi besar dan bahan berkualitas. Cek Instagram kami untuk info lebih lanjut!",
-        link: "https://www.instagram.com/kebabmonster_/"
-    },
+  {
+    index: 0,
+    image: sariroti,
+    title: "Sari Roti",
+    description:
+      "Sari Roti produces and distributes a variety of high-quality, hygienic, and affordable halal breads for all Indonesian consumers.",
+    link: "https://www.sariroti.com/id",
+  },
+  {
+    index: 1,
+    image: byU,
+    title: "By.U",
+    description:
+      "by.U is a fully digital internet SIM card from Telkomsel. All services are available in one app. Just open the by.U app to connect with the people and things you love.",
+    link: "https://www.byu.id/v2",
+  },
+  {
+    index: 2,
+    image: bumiaksara,
+    title: "Bumi Aksara",
+    description:
+      "PT Bumi Aksara is a national-scale publishing company that has been trusted to publish quality books for over three decades.",
+    link: "https://bumiaksaraonline.com/Tentang-Kami",
+  },
+  {
+    index: 3,
+    image: buynana,
+    title: "Buynana Chips",
+    description:
+      "Buynana Chips is a specialty banana chips product from Cianjur, available in five flavors: Chocolate, Milk, Strawberry, Original, and Green Tea.",
+    link: "https://buynanachips.com/",
+  },
+  {
+    index: 4,
+    image: monarq,
+    title: "Monarq Studio",
+    description:
+      "Monarq Studio is a creative studio focused on innovative design and branding solutions for various industries.",
+    link: "https://www.instagram.com/monarqstudiom/",
+  },
+  {
+    index: 5,
+    image: bankBNI,
+    title: "Bank BNI",
+    description:
+      "Bank BNI is one of the leading banks in Indonesia, offering a wide range of financial services for individuals and corporations.",
+    link: "https://www.bni.co.id/en-us/",
+  },
+  {
+    index: 6,
+    image: bankBRI,
+    title: "Bank BRI",
+    description:
+      "Bank BRI is a bank that provides integrated banking services, focusing on empowering SMEs in Indonesia.",
+    link: "https://bri.co.id",
+  },
+  {
+    index: 7,
+    image: bankBTN,
+    title: "Bank BTN",
+    description:
+      "Bank BTN is a bank known as a financial institution for housing in Indonesia, offering various banking products and services.",
+    link: "https://www.btn.co.id",
+  },
+  {
+    index: 8,
+    image: bankDKI,
+    title: "Bank DKI",
+    description:
+      "Bank DKI is a regional bank operating in Jakarta, providing banking services for individuals and businesses.",
+    link: "https://www.bankdki.co.id",
+  },
+  {
+    index: 9,
+    image: baznas,
+    title: "BAZNAS RI",
+    description:
+      "BAZNAS is the national zakat management agency tasked with managing and distributing zakat, infaq, and charity for the welfare of society.",
+    link: "https://baznas.go.id",
+  },
+  {
+    index: 10,
+    image: antangin,
+    title: "Antangin",
+    description:
+      "Antangin provides herbal health products to boost immunity and provide relief.",
+    link: "https://www.deltomed.id",
+  },
+  {
+    index: 11,
+    image: aliansi,
+    title: "Aliansi Kebangsaan",
+    description:
+      "Aliansi Kebangsaan promotes national unity and social development.",
+    link: "https://www.aliansikebangsaan.org/amp/",
+  },
+  {
+    index: 12,
+    image: makarizo,
+    title: "Makarizo",
+    description: "Makarizo is known for its high-quality hair care products.",
+    link: "https://makarizo.com",
+  },
+  {
+    index: 13,
+    image: mujigae,
+    title: "Mujigae",
+    description: "Mujigae offers a range of delicious Korean food products.",
+    link: "https://wonhaefoods.com/wonhae-series/",
+  },
+  {
+    index: 14,
+    image: momita,
+    title: "Momita Catering",
+    description:
+      "Momita Catering offers a variety of delicious catering services for events.",
+    link: "https://www.instagram.com/momita_catering/",
+  },
+  {
+    index: 15,
+    image: weRent,
+    title: "We.Rent",
+    description:
+      "We.Rent is a rental platform for various products and services, offering convenience for renting according to your needs.",
+    link: "https://www.instagram.com/we.rentjakarta/",
+  },
+  {
+    index: 16,
+    image: kebab,
+    title: "Kebab Monster",
+    description:
+      "Kebab Monster serves delicious kebabs with large portions and quality ingredients. Check our Instagram for more info!",
+    link: "https://www.instagram.com/kebabmonster_/",
+  },
+  {
+    index: 17,
+    image: kemkomarvest,
+    title: "Kementerian Koordinator Bidang Kemaritiman dan Investasi",
+    description: "Oversees Indonesia's policies on maritime affairs, investment, environment, and tourism.",
+    link: "https://maritim.go.id/",
+  },
+  {
+    index: 18,
+    image: kemkominfo,
+    title: "Kementrian Komunikasi dan Informatika",
+    description: "Kementrian Komunikasi dan Informatika 2024",
+    link: "https://layanan.kominfo.go.id/",
+  },
+  {
+    index: 19,
+    image: esdm,
+    title: "Kementrian Energi dan Sumber daya Mineral",
+    description: "Kementrian Energi dan Sumber Daya Mineral Republik Indonesia",
+    link: "https://www.esdm.go.id/id/",
+  },
+  {
+    index: 20,
+    image: gpnuku,
+    title: "Gerakan Pemuda Nuku",
+    description: "Gerakan Pemuda Nuku",
+    link: "",
+  },
+  {
+    index: 21,
+    image: omg,
+    title: "OMG",
+    description:
+      "Get radiant skin with OMG Facial Treatment, enriched with Peach Extract and Vitamins B3, C, & E. Fresh, glowing, non-oily!",
+    link: "https://www.itsomgbeauty.com/",
+  },
+  {
+    index: 22,
+    image: sopikanternate,
+    title: "Sop Ikan Ternate",
+    description:
+      "A savory Indonesian fish soup with Ternate spices, served with rice and sambal. Perfect for seafood lovers!",
+    link: "https://www.instagram.com/sopikanternate/",
+  },
 ];
